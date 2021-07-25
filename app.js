d4d5df015181b75c2c5b96bd3221ff7eb9723008
imamzafar <73028559+imamzafar@users.noreply.github.com>
2021-07-25 18:27:23 +0530

    d

--- app.js
+++ app.js
@@ -34,10 +34,10 @@ app.get('/test', (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   // Step 1:
-  app.use(express.static(path.resolve(__dirname, "../client/build")));
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
   // Step 2:
   app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
   });
 }
 
